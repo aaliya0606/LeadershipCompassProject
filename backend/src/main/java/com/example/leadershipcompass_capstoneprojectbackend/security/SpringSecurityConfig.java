1ebe6e2753bcda78663e44ae74e4b6c7aa2e64dd
a.cheskaa_ <cheska_mascarinas@yahoo.com>
2026-08-26 21:23:45 +1000

    refining database (new version of setup)

--- backend/src/main/java/com/example/leadershipcompass_capstoneprojectbackend/security/SpringSecurityConfig.java
+++ backend/src/main/java/com/example/leadershipcompass_capstoneprojectbackend/security/SpringSecurityConfig.java
@@ -79,7 +79,8 @@ public class SpringSecurityConfig {
                 "http://localhost:3000",
                 "http://127.0.0.1:5500",
                 "http://localhost:5500",
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "http://localhost:63342"
         ));
 
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
