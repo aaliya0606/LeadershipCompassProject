886d35403e7ebbeceb58ac4662e775feaaa3f59f
a.cheskaa_ <cheska_mascarinas@yahoo.com>
2026-08-26 13:52:04 +1000

    fixing database and frontend framework

--- backend/src/main/java/com/example/leadershipcompass_capstoneprojectbackend/LeadershipCompassCapstoneProjectBackendApplication.java
+++ backend/src/main/java/com/example/leadershipcompass_capstoneprojectbackend/LeadershipCompassCapstoneProjectBackendApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LeadershipCompassCapstoneProjectBackendApplication {
+public class postLeadershipCompassCapstoneProjectBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LeadershipCompassCapstoneProjectBackendApplication.class, args);
